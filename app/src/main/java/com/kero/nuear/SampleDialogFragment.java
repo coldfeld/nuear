075f342754dbea9b5fb60a81788d9dd4a92a1f0d
coldfeld <coldfeld@ya.ru>
2018-08-02 22:54:48 +0300

    blur fragment class

---
+++ app/src/main/java/com/kero/nuear/SampleDialogFragment.java
@@ -0,0 +1,53 @@
+package com.kero.nuear;
+
+import android.os.Bundle;
+
+import fr.tvbarthel.lib.blurdialogfragment.BlurDialogFragment;
+
+public class SampleDialogFragment extends BlurDialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        ...
+    }
+
+    @Override
+    protected float getDownScaleFactor() {
+        // Allow to customize the down scale factor.
+        return (float) 5.0;
+    }
+
+    @Override
+    protected int getBlurRadius() {
+        // Allow to customize the blur radius factor.
+        return 7;
+    }
+
+    @Override
+    protected boolean isActionBarBlurred() {
+        // Enable or disable the blur effect on the action bar.
+        // Disabled by default.
+        return true;
+    }
+
+    @Override
+    protected boolean isDimmingEnable() {
+        // Enable or disable the dimming effect.
+        // Disabled by default.
+        return true;
+    }
+
+    @Override
+    protected boolean isRenderScriptEnable() {
+        // Enable or disable the use of RenderScript for blurring effect
+        // Disabled by default.
+        return true;
+    }
+
+    @Override
+    protected boolean isDebugEnable() {
+        // Enable or disable debug mode.
+        // False by default.
+        return true;
+    }
+}
--- app/src/main/java/com/kero/nuear/settings.java
+++ app/src/main/java/com/kero/nuear/settings.java
@@ -2,14 +2,16 @@ package com.kero.nuear;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.constraint.ConstraintLayout;
 
 public class settings extends Activity {
 
+    ConstraintLayout lang_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+    }
 }
